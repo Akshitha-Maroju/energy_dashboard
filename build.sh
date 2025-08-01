7db5ec7c04bff4d85daffb378b6ba0f461463738
Akshitha-Maroju <akshithamaroju02@gmail.com>
2025-08-01 14:04:35 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-# Install all required Python packages
+# Make sure setuptools and wheel are available for building packages
+pip install --upgrade pip setuptools wheel
+
+# Install app dependencies
 pip install -r requirements.txt
+
 
